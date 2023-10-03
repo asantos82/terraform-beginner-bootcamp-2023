1b61b1c10674ee4990f3ae437f846461fd428016
Andre Santos <dre.santos@gmail.com>
2023-10-03 23:26:11 +0000

    Create a test s3 bucket

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
   }
 }
 
@@ -15,6 +19,12 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  lower = true
+  upper = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
